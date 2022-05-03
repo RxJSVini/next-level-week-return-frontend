bc1db68dc570761a0b57a1f7e5f1d8edb3461c13
Vinicius Pereira <Vinicius.Pereira@processor.com.br>
2022-05-02 21:47:33 -0300

    Primeira Aula da NLW

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -12,7 +12,7 @@ const Widget: React.FC = () => {
     return (
         <Popover>
             <Popover.Button className="absolute bottom-4 right-4">
-                <Popover.Panel>OláMundo!</Popover.Panel>
+                <Popover.Panel>Olá Mundo!</Popover.Panel>
                 <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group" onClick={toggleWidgetVisibility}>
                     <ChatTeardropDots className="w-6 h-6" />
                     <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
